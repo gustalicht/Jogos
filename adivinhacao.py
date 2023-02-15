ba48f4901d0534eb1402c1f1146ab974e207351e
gustalicht <gugapaynes@hotmail.com>
2023-02-15 12:26:42 -0300

    Arquivos de projeto do PythonJogos

---
+++ adivinhacao.py
@@ -0,0 +1,9 @@
+print ("*********************************")
+print ("bem vindo ao jogoo de adivinhação")
+print ("*********************************")
+
+numero_secreto= 42
+
+chute = input ("digite o seu numero:")
+
+print ("você digitou", chute)
